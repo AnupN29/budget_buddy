fc4a6526ba54b0a66e12811264b5de3d3292eadd
AnupN29 <nagdeveanup29@gmail.com>
2024-01-07 15:52:57 +0530

    ADDED CRUD on users & transactions

--- backend/app/models/database.py
+++ backend/app/models/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:toor@localhost:3306/Personal_Finance_App'
+SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:toor@localhost:3306/BudgetApp'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
